58d7e3be5ac325f08b4e7c7e31023085e5081f44
Jibi Jose <jibijose@yahoo.com>
2020-08-10 14:18:56 +0530

    couchbase k8s cluster

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAMESPACE=couchbase
+kubectl create namespace ${NAMESPACE}
+
+unzip couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64.zip
+cd couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
+kubectl create -f crd.yaml
+bin/cbopcfg --no-operator --namespace ${NAMESPACE} | kubectl create -n ${NAMESPACE} -f -
+bin/cbopcfg --no-admission --namespace ${NAMESPACE} | kubectl create -n ${NAMESPACE} -f -
+cd ..
+rm -rf couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
+
+#kubectl get deployments -n ${NAMESPACE}  
+### https://docs.couchbase.com/operator/2.0/reference-couchbasecluster.html
+kubectl create -f couchbase-cluster.yaml -n ${NAMESPACE}
+#kubectl get pods -n ${NAMESPACE}
+#kubectl port-forward cb-example-0000 8091 -n ${NAMESPACE}
+#kubectl get svc -n ${NAMESPACE}
+
---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAMESPACE=couchbase
+
+kubectl delete -f couchbase-cluster.yaml -n ${NAMESPACE}
+
+unzip couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64.zip
+cd couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
+bin/cbopcfg --no-operator --namespace ${NAMESPACE} | kubectl delete -n ${NAMESPACE} -f -
+bin/cbopcfg --no-admission --namespace ${NAMESPACE} | kubectl delete -n ${NAMESPACE} -f -
+kubectl delete -f crd.yaml
+cd ..
+
+kubectl delete namespace ${NAMESPACE}
