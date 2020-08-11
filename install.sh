7c826c04b81983a088f88bf1f69d1a8de13ef46d
Jibi Jose <jibijose@yahoo.com>
2020-08-11 20:50:57 +0530

    helm chart working

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 NAMESPACE=couchbase
+
+echo "******************************************************************************************"
+echo `date`"   Creating namespace ${NAMESPACE}"
 kubectl create namespace ${NAMESPACE}
 
+echo "******************************************************************************************"
+echo `date`"   Adding couchbase kubernetes operators to namespace ${NAMESPACE}"
 unzip -qq couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64.zip
 cd couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
 kubectl create -f crd.yaml
@@ -11,9 +16,14 @@ bin/cbopcfg --no-admission --namespace ${NAMESPACE} | kubectl create -n ${NAMESP
 cd ..
 rm -rf couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
 
+echo "******************************************************************************************"
+echo `date`"   Applying couchbase cluster helm chart to namespace ${NAMESPACE}"
 #kubectl get deployments -n ${NAMESPACE}  
 ### https://docs.couchbase.com/operator/2.0/reference-couchbasecluster.html
-kubectl create -f couchbase-cluster.yaml -n ${NAMESPACE}
+#kubectl create -f couchbase-cluster.yaml -n ${NAMESPACE}
+#helm install couchbase-cluster --dry-run --debug ./couchbase-cluster -n ${NAMESPACE}
+helm install couchbase-cluster ./couchbase-cluster -n ${NAMESPACE}
+#helm ls -n ${NAMESPACE}
 #kubectl get pods -n ${NAMESPACE}
 #kubectl port-forward cb-example-0000 8091 -n ${NAMESPACE}
 #kubectl get svc -n ${NAMESPACE}
--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,13 @@
 
 NAMESPACE=couchbase
 
-kubectl delete -f couchbase-cluster.yaml -n ${NAMESPACE}
+echo "******************************************************************************************"
+echo `date`"   Uninstalling couchbase cluster helm chart to namespace ${NAMESPACE}"
+#kubectl delete -f couchbase-cluster.yaml -n ${NAMESPACE}
+helm uninstall couchbase-cluster -n ${NAMESPACE}
 
+echo "******************************************************************************************"
+echo `date`"   Removing couchbase kubernetes operators to namespace ${NAMESPACE}"
 unzip -qq couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64.zip
 cd couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
 bin/cbopcfg --no-operator --namespace ${NAMESPACE} | kubectl delete -n ${NAMESPACE} -f -
@@ -12,4 +17,6 @@ kubectl delete -f crd.yaml
 cd ..
 rm -rf couchbase-autonomous-operator-kubernetes_2.0.1-macos-x86_64
 
+echo "******************************************************************************************"
+echo `date`"   Deleting namespace ${NAMESPACE}"
 kubectl delete namespace ${NAMESPACE}
